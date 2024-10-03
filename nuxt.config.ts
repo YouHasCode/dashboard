9e066315cadf9b06a5ad35e485c90433b2c2ce5a
youssef hasnaoui <hasnaouiyoussef@proton.me>
2024-10-03 19:06:40 +0200

    first init <3

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/i18n'
+  ],
+  i18n: {
+    strategy: 'prefix_except_default',
+    lazy: true,
+    langDir: 'locales',
+    defaultLocale: 'en',
+    locales: [
+      {
+        name: 'English',
+        code: 'en',
+        file: 'en.json'
+      }
+    ],
+    experimental: {
+      localeDetector: './localeDetector.ts'
+    }
+  },
+  build: {
+    transpile: [
+      'trpc-nuxt'
+    ]
+  },
+  compatibilityDate: '2024-08-19',
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    shim: false
+  }
+})
